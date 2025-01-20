5293571b0845f147142baefcd4eba4ba0a0e78e6
Harshita Pande <2004pandeharshita@gmail.com>
2025-01-20 18:30:36 +0530

    Add Linear_reg.py and other updates

---
+++ Linear_reg.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn import linear_model
+
+#Creating Dataset
+data= { 'Area':[2600,3000,3200,3600,4000],'Price':[550000,565000,610000,680000,725000]}
+df = pd.DataFrame(data)
+print(df)
+
+# Plotting the data points
+plt.xlabel('Area(sq ft)')
+plt.ylabel('Price(US $)')
+plt.scatter(df.Area,df.Price,color='red',marker='+')
+plt.show()
+
+#Defining the Model
+reg = linear_model.LinearRegression()
+reg.fit(df[['Area']],df.Price)
+
+#Predicting Variable 
+prediction=reg.predict(pd.DataFrame({'Area':[3300]}))
+
+#Making predictions
+print(f"The Price of house with 3300 sq. ft. area is ${prediction[0]}")
+
+#MANNUAL CALCULATIONS 
+
+# Slope(m) [change in y for per unit change in x] 
+m=reg.coef_
+print(f"Slope of regression line : {m[0]}")
+
+#intercept(b) [value of y for x=0]
+b=reg.intercept_
+print(f"The intercept at Y-axis : {b}")
+
+#manual prediction [y=mx+b]
+prediction_manual= m*3300 + b 
+print(f"The cost of house with 3300 sq. ft. area is {prediction_manual[0]}")
+
+file=pd.read_excel("house_price.xlsx")
